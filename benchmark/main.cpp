1ff06e2c915e75adf4fb53b56589b90eef08e22d
JennyGit-PC <674886730@qq.com>
2021-03-28 16:46:08 +0800

    Change the config of benchmark tests.

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -12,9 +12,15 @@
 #    include <unistd.h>
 #endif // _MSC_VER
 
+#define LOG_COUNT    100000
+#define THREAD_COUNT 1
+#define ASYNC_SIZE   (1024 * 1024)
+#define ROLL_SIZE    (1024 * 1024 * 10)
+
 #define INIT_LOG_CONFIGS(async_buffer_size, roll_size)                         \
     do                                                                         \
     {                                                                          \
+        system("rm -f /var/log/ezlog_benchmark_test/*");                       \
         ezlog_init();                                                          \
         ezlog_set_log_roll_enabled((roll_size) > 0);                           \
         ezlog_set_log_color_enabled(true);                                     \
@@ -33,27 +39,33 @@
         INIT_LOG_CONFIGS(async_buffer_size, roll_size);                        \
         for (auto _ : state)                                                   \
         {                                                                      \
-            LOG_HEX(g_test_bytes, sizeof(g_test_bytes));                       \
+            for (unsigned int i = 0; i < LOG_COUNT; i++)                       \
+            {                                                                  \
+                LOG_HEX(g_test_bytes, sizeof(g_test_bytes));                   \
+            }                                                                  \
         }                                                                      \
     }                                                                          \
-    BENCHMARK(name##_hex)
+    BENCHMARK(name##_hex)->Unit(benchmark::kMillisecond)->Threads(THREAD_COUNT)
 
-#define DECLARE_BENCHMARK_LOG_FORMAT_FUNCTION(                                 \
-    name,                                                                      \
-    async_buffer_size,                                                         \
-    roll_size)                                                                 \
+#define DECLARE_BENCHMARK_LOG_FMT_FUNCTION(name, async_buffer_size, roll_size) \
     static void name##_formated(benchmark::State& state)                       \
     {                                                                          \
         INIT_LOG_CONFIGS(async_buffer_size, roll_size);                        \
         for (auto _ : state)                                                   \
         {                                                                      \
-            LOG_DEBUG("test debug log: %d", 123456);                           \
+            for (unsigned int i = 0; i < LOG_COUNT; i++)                       \
+            {                                                                  \
+                /* 100 bytes. */                                               \
+                LOG_DEBUG("test debug log: %d", 123456789);                    \
+                LOG_DEBUG("test debug log: %d", 123456789);                    \
+                LOG_DEBUG("test debug log: %d", 123456789);                    \
+                LOG_DEBUG("test debug log: %d", 123456789);                    \
+            }                                                                  \
         }                                                                      \
     }                                                                          \
-    BENCHMARK(name##_formated)
-
-#define ASYNC_SIZE (1024 * 1024)
-#define ROLL_SIZE  (1024 * 1024 * 10)
+    BENCHMARK(name##_formated)                                                 \
+        ->Unit(benchmark::kMillisecond)                                        \
+        ->Threads(THREAD_COUNT)
 
 static unsigned char g_test_bytes[39] = {0};
 
@@ -62,7 +74,11 @@ static const char* get_output_path_hook()
     static unsigned int index     = 0;
     static char         path[256] = {0};
 
-    snprintf(path, sizeof(path), "/var/log/test_%d.log", index++);
+    snprintf(
+        path,
+        sizeof(path),
+        "/var/log/ezlog_benchmark_test/%d.log",
+        index++);
 
     return path;
 }
@@ -72,9 +88,9 @@ DECLARE_BENCHMARK_LOG_HEX_FUNCTION(sync_roll_log, 0, ROLL_SIZE);
 DECLARE_BENCHMARK_LOG_HEX_FUNCTION(async_log, ASYNC_SIZE, 0);
 DECLARE_BENCHMARK_LOG_HEX_FUNCTION(async_roll_log, ASYNC_SIZE, ROLL_SIZE);
 
-DECLARE_BENCHMARK_LOG_FORMAT_FUNCTION(sync_log, 0, 0);
-DECLARE_BENCHMARK_LOG_FORMAT_FUNCTION(sync_roll_log, 0, ROLL_SIZE);
-DECLARE_BENCHMARK_LOG_FORMAT_FUNCTION(async_log, ASYNC_SIZE, 0);
-DECLARE_BENCHMARK_LOG_FORMAT_FUNCTION(async_roll_log, ASYNC_SIZE, ROLL_SIZE);
+DECLARE_BENCHMARK_LOG_FMT_FUNCTION(sync_log, 0, 0);
+DECLARE_BENCHMARK_LOG_FMT_FUNCTION(sync_roll_log, 0, ROLL_SIZE);
+DECLARE_BENCHMARK_LOG_FMT_FUNCTION(async_log, ASYNC_SIZE, 0);
+DECLARE_BENCHMARK_LOG_FMT_FUNCTION(async_roll_log, ASYNC_SIZE, ROLL_SIZE);
 
 BENCHMARK_MAIN();
