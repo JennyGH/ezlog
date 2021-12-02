274f7bf1719e1cb751038e2423abcb086cf916c7
dubingjian <dubingjian@cnca.net>
2021-12-02 17:37:06 +0800

    fix: 增加私有命名空间限制

--- src/platform_compatibility.cpp
+++ src/platform_compatibility.cpp
@@ -13,121 +13,125 @@
 
 #    define __ASSERT(expr) __ASSERT_WITH_FILE(expr, file, line)
 
-static void
-_default_assert_hook(const char* expr, const char* file, unsigned int line)
+namespace ezlog
 {
-    fprintf(
-        stderr,
-        "Assertion failed: %s, at file: \"%s\", line: %u",
-        expr,
-        file,
-        line);
-}
-
-assert_hook_t g_assert_hook = _default_assert_hook;
-
-void set_assert_hook(assert_hook_t hook)
-{
-    if (NULL != hook)
+    static void
+    _default_assert_hook(const char* expr, const char* file, unsigned int line)
     {
-        g_assert_hook = hook;
+        fprintf(
+            stderr,
+            "Assertion failed: %s, at file: \"%s\", line: %u",
+            expr,
+            file,
+            line);
     }
-}
+
+    assert_hook_t g_assert_hook = _default_assert_hook;
+
+    void set_assert_hook(assert_hook_t hook)
+    {
+        if (NULL != hook)
+        {
+            g_assert_hook = hook;
+        }
+    }
 
 #    ifdef __no_memcpy_s
-void __memcpy_s(
-    const char* file,
-    size_t      line,
-    void*       dest,
-    size_t      dest_size,
-    const void* src,
-    size_t      src_size)
-{
-    __ASSERT((dest != NULL) && (src != NULL));
-    __ASSERT(dest_size >= src_size);
-    memcpy(dest, src, src_size);
-}
+    void __memcpy_s(
+        const char* file,
+        size_t      line,
+        void*       dest,
+        size_t      dest_size,
+        const void* src,
+        size_t      src_size)
+    {
+        __ASSERT((dest != NULL) && (src != NULL));
+        __ASSERT(dest_size >= src_size);
+        memcpy(dest, src, src_size);
+    }
 #    endif // __no_memcpy_s
 
 #    ifdef __no_vsprintf_s
-int __vsprintf_s(
-    const char* file,
-    size_t      line,
-    char*       dest,
-    size_t      dest_size,
-    const char* format,
-    va_list     args)
-{
-    __ASSERT(dest != NULL);
-    __ASSERT(dest_size > 0);
-    __ASSERT(format != NULL);
-    int sprintfed_size = vsnprintf(dest, dest_size, format, args);
-    __ASSERT(dest_size >= sprintfed_size);
-    return sprintfed_size;
-}
+    int __vsprintf_s(
+        const char* file,
+        size_t      line,
+        char*       dest,
+        size_t      dest_size,
+        const char* format,
+        va_list     args)
+    {
+        __ASSERT(dest != NULL);
+        __ASSERT(dest_size > 0);
+        __ASSERT(format != NULL);
+        int sprintfed_size = vsnprintf(dest, dest_size, format, args);
+        __ASSERT(dest_size >= sprintfed_size);
+        return sprintfed_size;
+    }
 #    endif // __no_vsprintf_s
 
 #    ifdef __no_sprintf_s
-int __sprintf_s(
-    const char* file,
-    size_t      line,
-    char*       dest,
-    size_t      dest_size,
-    const char* format,
-    ...)
-{
+    int __sprintf_s(
+        const char* file,
+        size_t      line,
+        char*       dest,
+        size_t      dest_size,
+        const char* format,
+        ...)
+    {
 
-    va_list args;
-    va_start(args, format);
-    int rc = __vsprintf_s(file, line, dest, dest_size, format, args);
-    va_end(args);
-    return rc;
-}
+        va_list args;
+        va_start(args, format);
+        int rc = __vsprintf_s(file, line, dest, dest_size, format, args);
+        va_end(args);
+        return rc;
+    }
 #    endif // __no_sprintf_s
 
 #    ifdef __no_sscanf_s
-int __sscanf_s(
-    const char* file,
-    size_t      line,
-    const char* buffer,
-    const char* format,
-    ...)
-{
-    __ASSERT(buffer != NULL);
-    __ASSERT(format != NULL);
-    va_list args;
-    va_start(args, format);
-    int sscanfed_size = vsscanf(buffer, format, args);
-    va_end(args);
-    return sscanfed_size;
-}
+    int __sscanf_s(
+        const char* file,
+        size_t      line,
+        const char* buffer,
+        const char* format,
+        ...)
+    {
+        __ASSERT(buffer != NULL);
+        __ASSERT(format != NULL);
+        va_list args;
+        va_start(args, format);
+        int sscanfed_size = vsscanf(buffer, format, args);
+        va_end(args);
+        return sscanfed_size;
+    }
 #    endif // __no_sscanf_s
 
 #    ifdef __no_strcpy_s
-int __strcpy_s(
-    const char* file,
-    size_t      line,
-    char*       dest,
-    size_t      dest_size,
-    const char* src)
-{
-    __ASSERT((dest != NULL) && (src != NULL));
-    __ASSERT(dest_size > 0);
-    size_t src_size = ::strlen(src) + 1;
-    __ASSERT(dest_size >= src_size);
-    strcpy(dest, src);
-    return src_size;
-}
+    int __strcpy_s(
+        const char* file,
+        size_t      line,
+        char*       dest,
+        size_t      dest_size,
+        const char* src)
+    {
+        __ASSERT((dest != NULL) && (src != NULL));
+        __ASSERT(dest_size > 0);
+        size_t src_size = ::strlen(src) + 1;
+        __ASSERT(dest_size >= src_size);
+        strcpy(dest, src);
+        return src_size;
+    }
 #    endif // __no_strcpy_s
 
-pid_t __gettid()
-{
-    static __thread pid_t tid = 0;
-    if (tid == 0)
+    pid_t __gettid()
     {
-        tid = syscall(SYS_gettid);
+        static __thread pid_t tid = 0;
+        if (tid == 0)
+        {
+            tid = syscall(SYS_gettid);
+        }
+        return tid;
     }
-    return tid;
-}
+
+} // namespace ezlog
 
 #endif // !_MSC_VER
